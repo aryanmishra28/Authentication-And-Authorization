4f441b2a497a71d847f5289e62c08cdcf7e88e04
aryanmishra28 <aryanmishra1851@gmail.com>
2025-07-09 18:36:46 +0530

    cookies,jwt,bcrypt

--- app.js
+++ app.js
@@ -1,37 +1,65 @@
 const express = require('express');
 const app= express();
 const bcrypt= require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 
-
-//ENCRYPTING PASSWORDS
+//JWT AUTHENTICATION
 app.get('/', function(req, res){
-    bcrypt.genSalt(10, function(err, salt) {
-        // console.log('Salt:', salt); //to see the generated salt
-        bcrypt.hash('Password123', salt, function(err, hash) {
-            // Store hash in your password DB.
-            console.log('Hash:', hash); //hashed password, which is Hash: $2b$10$GulGM49GXj69aRK30T19JulVoCcPPbC0p45FHfPaKHILN1/Q2N/ba
-            res.send('Password hashed successfully!');
-        });
-    });
-})
-
-
-//VERIFYING PASSWORDS
-app.get('/verify', function(req, res){
-    bcrypt.compare('Password123', '$2b$10$GulGM49GXj69aRK30T19JulVoCcPPbC0p45FHfPaKHILN1/Q2N/ba', function(err, result) {
-        // result == true
-        console.log('Password match:', result); //true if password matches
-        res.send('Password verification complete!');
-    });
+    let token = jwt.sign({user: "exampleUser@gmail.com"}, "Secret");
+    res.cookie('token', token);
+    res.send('Cookie has been set with JWT token!');
 });
 
+app.get('/read', function(req, res) {
+    console.log(req.cookies); // Access the cookie named 'token'
+});
+
+app.get('/verify', function(req, res) {
+   let data =  jwt.verify(req.cookies.token, "Secret")
+    console.log(data);
+});
 
 app.listen(3000);
 
 
 
 
+
+
+
+
+
+
+
+
+// //ENCRYPTING PASSWORDS
+// app.get('/', function(req, res){
+//     bcrypt.genSalt(10, function(err, salt) {
+//         // console.log('Salt:', salt); //to see the generated salt
+//         bcrypt.hash('Password123', salt, function(err, hash) {
+//             // Store hash in your password DB.
+//             console.log('Hash:', hash); //hashed password, which is Hash: $2b$10$GulGM49GXj69aRK30T19JulVoCcPPbC0p45FHfPaKHILN1/Q2N/ba
+//             res.send('Password hashed successfully!');
+//         });
+//     });
+// })
+
+
+// //VERIFYING PASSWORDS
+// app.get('/verify', function(req, res){
+//     bcrypt.compare('Password123', '$2b$10$GulGM49GXj69aRK30T19JulVoCcPPbC0p45FHfPaKHILN1/Q2N/ba', function(err, result) {
+//         // result == true
+//         console.log('Password match:', result); //true if password matches
+//         res.send('Password verification complete!');
+//     });
+// });
+
+
+// app.listen(3000);
 
 
 
